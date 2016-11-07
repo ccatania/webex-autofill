94d648bff7c4c01c6bf339bb07c02e8d7c5f1f2b
Constanza Catania <connie.catania@gmail.com>
2016-11-07 12:40:09 -0300

    Autofill for webex meeting and webex room

--- webexAutofill.js
+++ webexAutofill.js
@@ -7,6 +7,12 @@ chrome.storage.local.get(null, (res) => {
   if (res.email) {
     email = res.email;
   }
-  document.getElementById('attendeeName').value = name;
-  document.getElementById('attendeeEmail').value = email;
+
+  if (document.getElementById('attendeeName') !== null && document.getElementById('attendeeEmail') !== null) {
+    document.getElementById('attendeeName').value = name;
+    document.getElementById('attendeeEmail').value = email;
+  } else if (document.getElementById('mc-input-screenname') !== null && document.getElementById('mc-input-email') !== null) {
+    document.getElementById('mc-input-screenname').value = name;
+    document.getElementById('mc-input-email').value = email;
+  }
 });
